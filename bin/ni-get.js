d134724bd7ff3b62c552f7a51608aa5ad553eca5
Max Stockner <mail@bluemaex.de>
2016-02-07 02:27:42 +0100

    only download files that are not available locally

--- bin/ni-get.js
+++ bin/ni-get.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 "use strict"
 
+var fs      = require('fs')
+var crypto  = require('crypto')
+var async   = require('async')
 var program = require('commander')
 var Loader  = require('../lib/loader')
 var HMap    = require('../lib/hmap')
@@ -21,11 +24,41 @@ if(program.args < 1) {
 
 function loadIndex(filename, cb) {
 	var wishlist = new HMap()
-	
+
 	util.parseIndex(filename, function(path, hash) {
 		wishlist.add(path, hash.toString('hex'))
 	}, function() {
 		cb(wishlist)
+	})
+}
+
+function checkExistingFiles(wishlist, destination, callback) {
+	async.eachLimit(wishlist.paths(), 1, function(item, cb) {
+		var hash = crypto.createHash('sha256')
+		var file = fs.createReadStream(destination + '/' + item)
+
+		file.on("error", function() {
+			cb()
+		})
+		file.pipe(hash, { end: false })
+		file.on('end', function () {
+			hash.end()
+
+			var local_hash = hash.read().toString('hex')
+			var index_hash = wishlist.byPath(item)
+
+			if(local_hash === index_hash) {
+				wishlist.removePath(item)
+			}
+
+			cb()
+		})
+	}, function(err){
+		if(err) {
+			console.log(err)
+		}
+
+		callback(wishlist)
 	})
 }
 
@@ -35,7 +68,9 @@ function main(options) {
 		servers = options.server.split(',')
 	}
 	loadIndex(program.args[0], function(wishlist) {
-		var l = new Loader(wishlist, options.destination, servers)
+		checkExistingFiles(wishlist, options.destination, function(bla) {
+			var l = new Loader(bla, options.destination, servers)
+		})
 	})
 }
 
